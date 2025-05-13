247f44ceccd0866b4208e00de3f739cfc583668f
jlndvr <landjosueo@gmail.com>
2025-05-13 18:53:53 -0500

    First Commit

---
+++ src/main/java/com/example/mySpringProject/MySpringProjectApplication.java
@@ -0,0 +1,12 @@
+package com.example.mySpringProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringProjectApplication {
+    public static void main(String[] args) {
+        System.out.println("🚀 Starting Spring Boot App...");
+        SpringApplication.run(MySpringProjectApplication.class, args);
+    }
+}
